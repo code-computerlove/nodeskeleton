38d1b7f50285249209b4893e150da43189f85c50
Tony McBeth <tony.mcbeth@gmail.com>
2017-05-02 16:58:08 +0100

    updated to ES6 syntax

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+const http = require('http');
 
 http.createServer(function (req, res) {
   console.log('Server listening on port 3000')
--- tests/test-framework.js
+++ tests/test-framework.js
@@ -1,8 +1,8 @@
-var chai = require('chai').should();
+const expect = require('chai').expect;
 
 
 describe('Test application framework is setup correctly', function(){
 	it('should not error', function(){
-		
+		expect(true).to.be.true;
 	});
 });
